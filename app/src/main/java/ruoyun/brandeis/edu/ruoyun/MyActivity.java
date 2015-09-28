d77a1d43328d923b951b700d888287a18df6761e
jasminesong <2837320441@qq.com>
2015-09-28 13:54:37 -0400

    submit modification of files before the offical tutorial until Getting Started Building your first App Starting another Activity Build Content

--- app/src/main/java/ruoyun/brandeis/edu/ruoyun/MyActivity.java
+++ app/src/main/java/ruoyun/brandeis/edu/ruoyun/MyActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -33,5 +36,17 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+
+
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
     }
 }
