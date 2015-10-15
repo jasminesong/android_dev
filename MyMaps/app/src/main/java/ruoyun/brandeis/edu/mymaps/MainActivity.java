ef321a4303d97f73fa5e92ca772d47215d244f7f
jasminesong <2837320441@qq.com>
2015-10-15 10:08:47 -0400

    first map display

--- MyMaps/app/src/main/java/ruoyun/brandeis/edu/mymaps/MainActivity.java
+++ MyMaps/app/src/main/java/ruoyun/brandeis/edu/mymaps/MainActivity.java
@@ -1,20 +1,34 @@
 package ruoyun.brandeis.edu.mymaps;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.ConnectionRequest;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 
 public class MainActivity extends AppCompatActivity {
 
     GoogleMap mMap;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(serviceOk()){
+            setContentView(R.layout.activity_map);
+            Toast.makeText(this,"Ready to map!",Toast.LENGTH_LONG).show();
+
+        }else{
+            setContentView(R.layout.activity_main);
+        }
     }
 
     @Override
@@ -37,5 +51,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean serviceOk(){
+
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if(isAvailable== ConnectionResult.SUCCESS){
+            return true;
+        }else if(GooglePlayServicesUtil.isUserRecoverableError(isAvailable)){
+            Dialog dialog =GooglePlayServicesUtil.getErrorDialog(isAvailable,this,ERROR_DIALOG_REQUEST);
+            dialog.show();
+
+        }else{
+            Toast.makeText(this,"Cannot connect to mapping service",Toast.LENGTH_LONG).show();
+        }
+
+
+        return false;
+
     }
 }
